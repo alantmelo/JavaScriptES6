434d0409cf279831104ef76f713a41f262c7636b
Alan Tavares de Melo <alantmelo@gmail.com>
2020-06-10 19:47:04 -0300

    request using axios

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
+import axios from 'axios';
+class Api
+{
+    static async getUser(name)
+    {
+        try {
+            const reponse = await axios.get(`https://api.github.com/users/${name}`);
+            console.log(reponse.data);
 
+        }catch (e){
+            console.warn(e)
+        }
+    }
 
+}
+Api.getUser('alantmelo');
