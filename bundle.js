e24eb5d46d44268683fc4a5b9b902f7adca7d934
Alan Tavares de Melo <alantmelo@gmail.com>
2020-06-09 22:40:52 -0300

    ES6 fundamentals

--- bundle.js
+++ bundle.js
@@ -1,3 +1,111 @@
 "use strict";
 
-alert('teste');
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var List = /*#__PURE__*/function () {
+  function List() {
+    _classCallCheck(this, List);
+
+    this.data = [];
+  }
+
+  _createClass(List, [{
+    key: "add",
+    value: function add(data) {
+      this.data.push(data);
+      console.log(this.data);
+    }
+  }]);
+
+  return List;
+}();
+
+var TodoList = /*#__PURE__*/function (_List) {
+  _inherits(TodoList, _List);
+
+  var _super = _createSuper(TodoList);
+
+  function TodoList() {
+    _classCallCheck(this, TodoList);
+
+    return _super.call(this);
+  }
+
+  return TodoList;
+}(List);
+
+var myList = new TodoList();
+
+document.getElementById('newTodo').onclick = function () {
+  return myList.add('Alan');
+};
+
+var array = [2, 5, 8, 9];
+var fruit = [{
+  name: 'apples',
+  quantity: 2
+}, {
+  name: 'bananas',
+  quantity: 0
+}, {
+  name: 'grapes',
+  quantity: 5
+}];
+var mapped = array.map(function (e, index) {
+  console.log(index);
+  return e * -2;
+});
+var sum = array.reduce(function (total, next) {
+  return total + next;
+});
+var even = array.filter(function (e) {
+  return e % 2 == 0;
+});
+var found = fruit.find(function (e) {
+  return e.name == 'apples';
+});
+console.log(mapped);
+console.log(sum);
+console.log(even);
+console.log(found);
+var person = {
+  'name': 'Alan',
+  'age': 30,
+  'address': {
+    'street': 'Snowball',
+    'city': 'Coquitlam',
+    'country': 'Canada'
+  }
+};
+var name = person.name,
+    age = person.age,
+    country = person.address.country; // console.log(`${name}, ${age} years old, lives in ${country}.`);
+
+var returnText = function returnText(_ref) {
+  var name = _ref.name,
+      age = _ref.age,
+      country = _ref.address.country;
+  return console.log("".concat(name, ", ").concat(age, " years old, lives in ").concat(country, "."));
+};
+
+returnText(person);
--- main.js
+++ main.js
@@ -1,1 +1,74 @@
-alert('teste');
+class List
+{
+    constructor()
+    {
+        this.data = [];
+    }
+
+    add(data)
+    {
+        this.data.push(data);
+        console.log(this.data);
+    }
+
+}
+class TodoList extends List
+{
+    constructor()
+    {
+        super();
+    }
+}
+
+var myList = new TodoList;
+
+document.getElementById('newTodo').onclick = () => myList.add('Alan');
+
+
+let array = [2,5,8,9];
+
+const fruit = [
+    {name: 'apples', quantity: 2},
+    {name: 'bananas', quantity: 0},
+    {name: 'grapes', quantity: 5}
+];
+
+const mapped = array.map(function (e, index) {
+    console.log(index);
+    return e * -2;
+});
+
+const sum = array.reduce(function (total, next) {
+    return total + next;
+});
+
+const even = array.filter((e)=>{
+    return e % 2 == 0;
+})
+
+const found = fruit.find(e => e.name == 'apples');
+
+console.log(mapped);
+console.log(sum);
+console.log(even);
+console.log(found);
+
+const person = {
+    'name':'Alan',
+    'age':30,
+    'address': {
+        'street': 'Snowball',
+        'city': 'Coquitlam',
+        'country': 'Canada'
+    }
+}
+
+
+const { name, age, address: {  country } } = person;
+
+// console.log(`${name}, ${age} years old, lives in ${country}.`);
+
+const returnText = ({name, age, address: { country }}) => console.log(`${name}, ${age} years old, lives in ${country}.`);
+returnText(person);
+
+
